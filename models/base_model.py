d405cc0b7f7202c277ee56355386523d6a70b902
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-28 21:14:48 +0100

    4-re-create an instance with this dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,23 @@ import uuid
 class BaseModel:
     """Base model class, parent of other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize BaseModel attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return string representation of BaseModel instance"""
