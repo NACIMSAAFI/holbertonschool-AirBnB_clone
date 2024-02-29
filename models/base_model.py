d3c89004961fdce6a68a7a30bd2278cdcccd97fb
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-29 21:24:47 +0100

    5. Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.save(self)
+            storage.save()
 
     def __str__(self):
         """Return string representation of BaseModel instance"""
