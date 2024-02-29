453a04ba11d113523087dc9a90f2685d5ea21696
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-29 20:43:42 +0100

    5. Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,10 @@
 """
     BaseModel module
 """
+# models/base_model.py
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
@@ -26,6 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return string representation of BaseModel instance"""
@@ -35,6 +38,8 @@ class BaseModel:
     def save(self):
         """Update the updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
+        print("Objects saved to file.")
 
     def to_dict(self):
         """Return dictionary representation of BaseModel instance"""
