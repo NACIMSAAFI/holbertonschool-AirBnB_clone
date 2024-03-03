2b9c505b98cf54b948c88fc91e3869e17ed74c08
NACIMSAAFI <7715@holbertonstudents.com>
2024-03-03 21:30:36 +0100

    Add tests/test_models/test_engine/test_file_storage.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,42 +1,51 @@
 #!/usr/bin/python3
 """
-unit tests for the BaseModel
+testing the base model
 """
 import unittest
 from models.base_model import BaseModel
-from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
-    def setUp(self):
-        self.model = BaseModel()
+    def test_ing_init(self):
+        """
+        Test for __init__
+        """
+        instance = BaseModel()
+        self.assertIsNotNone(instance.id)
+        self.assertIsNotNone(instance.created_at)
+        self.assertIsNotNone(instance.updated_at)
 
-    def test_save(self):
-        """Test save method"""
-        self.model.save()
-        self.assertTrue(self.model.created_at < self.model.updated_at)
+    def test_ing_save(self):
+        """
+        Test for save
+        """
+        instance = BaseModel()
+        upat = instance.updated_at
+        cupat = instance.save()
+        self.assertNotEqual(upat, cupat)
 
-    def test_to_dict(self):
-        """Test to_dict method"""
-        model_dict = self.model.to_dict()
-        self.assertEqual(model_dict["__class__"], "BaseModel")
-        self.assertIsInstance(model_dict["created_at"], str)
-        self.assertIsInstance(model_dict["updated_at"], str)
+    def test_ing_to_dict(self):
+        """
+        Test for to_dict
+        """
+        instance = BaseModel()
+        mdict = instance.to_dict()
+        self.assertIsInstance(mdict, dict)
+        self.assertEqual(mdict["__class__"], 'BaseModel')
+        self.assertEqual(mdict['id'], instance.id)
+        self.assertEqual(mdict['created_at'], instance.created_at.isoformat())
+        self.assertEqual(mdict["updated_at"], instance.updated_at.isoformat())
 
-    def test_id(self):
-        """Test id attribute"""
-        self.assertTrue(hasattr(self.model, "id"))
-
-    def test_created_at(self):
-        """Test created_at attribute"""
-        self.assertTrue(hasattr(self.model, "created_at"))
-        self.assertIsInstance(self.model.created_at, datetime)
-
-    def test_str(self):
-        """Test __str__ method"""
-        expected_str = "[BaseModel] ({}) {}".format(self.model.id, self.model.__dict__)
-        self.assertEqual(str(self.model), expected_str)
+    def test_ing_str(self):
+        """
+        Test for string representation
+        """
+        instance = BaseModel()
+        self.assertTrue(str(instance).startswith('[BaseModel]'))
+        self.assertIn(instance.id, str(instance))
+        self.assertIn(str(instance.__dict__), str(instance))
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unitt
