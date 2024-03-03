92a93c33be3e0ac520f6cb043fe608a4fca0bc2b
NACIMSAAFI <7715@holbertonstudents.com>
2024-03-03 13:24:31 +0100

    Update file_storage task 10

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,6 +32,11 @@ class FileStorage:
         """Deserializes the JSON file to __objects"""
         from models.base_model import BaseModel
         from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
         if exists(self.__file_path):
             with open(self.__file_path, "r") as file:
